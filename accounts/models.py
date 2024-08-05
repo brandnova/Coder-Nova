0c5247cba9f3e6e2486910923ac066f5b124a559
Ijeoma Jahsway <uchennamebijay@gmail.com>
2024-08-05 23:45:56 +0100

    Minor mods here and there. Epic fail with trying to implement SQL Client database, i'll try again later.

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField  # type: ignore
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -23,3 +24,11 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+class InfoContent(models.Model):
+    title = models.CharField(max_length=200)
+    content = RichTextField()
+    section_id = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
